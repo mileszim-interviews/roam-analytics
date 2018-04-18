6137da33da51b4abc93ed89b0e6d0f66985d1650
Miles Zimmerman <mileszim@protonmail.com>
2018-04-18 14:10:45 -0700

    Deploying build to docs/ for gh-pages

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = `/${ENV.modulePrefix}`;
+    ENV.locationType = 'hash';
   }
 
   return ENV;
